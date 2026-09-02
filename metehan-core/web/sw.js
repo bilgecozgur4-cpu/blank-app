3726eeaad026c4b325d2af0901262f4bcbe95d8f
bilgecozgur4-cpu <bilgecozgur4@gmail.com>
2026-09-02 08:45:27 +0300

    METEHAN V0.5: refresh web shell cache

--- metehan-core/web/sw.js
+++ metehan-core/web/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kutalp-prime-v03-shell';
-const ASSETS = ['/', '/static/styles.css', '/static/app.js', '/static/icon.svg', '/manifest.webmanifest'];
+const CACHE = 'metehan-v05-shell';
+const ASSETS = ['/', '/static/styles.css', '/static/app.js', '/static/native.js', '/static/icon.svg', '/manifest.webmanifest'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener('fetch', e => {
